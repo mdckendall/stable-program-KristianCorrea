64bdaaaa3b4529005245d0e081b898ada511263c
Kristian Correa <62035025+KristianCorrea@users.noreply.github.com>
2023-02-28 04:05:05 +0000

    Formatting on Stable print func.

--- Stable.java
+++ Stable.java
@@ -1,16 +1,20 @@
 import java.util.ArrayList;//Create the Stable class here.
-public class Stable{
+
+public class Stable {
   String address;
   ArrayList<Horse> arrayList = new ArrayList<Horse>();
-  public Stable(String address){
+
+  public Stable(String address) {
     this.address = address;
   }
-  public Stable(){
+
+  public Stable() {
   }
-  
-  public void listHorses(){
-    for(int i =0 ; i < arrayList.size(); i++){
-      System.out.println("Horse #" + (i+1) + " Name: " +arrayList.get(i).name+ " Weight: " + arrayList.get(i).weight + " Tame: " +arrayList.get(i).tame);
+
+  public void listHorses() {
+    for (int i = 0; i < arrayList.size(); i++) {
+      System.out.println("Horse #" + (i + 1) + ", Name: " + arrayList.get(i).name + ", Weight: "
+          + arrayList.get(i).weight + ", Tame: " + arrayList.get(i).tame);
     }
   }
 }
